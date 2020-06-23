5d35a99ee4f6f9e9379513461bcb64613f7c4de7
hua_zhenguo <hua_zhenguo@163.com>
2020-06-23 10:02:13 +0800

    修改

--- tool/base.sh
+++ tool/base.sh
@@ -6,9 +6,8 @@ alias enter=$current_path/tool/exec-docker.sh $1
 
 alias c='clear'
 
-# source $current_path/project/manage.sh
-
-# source $current_path/project/zrcaifu.sh
+source $current_path/project/manage.sh
+source $current_path/project/zrcaifu.sh
 
 alias status="$current_path/tool/git/git.sh status"
 alias pull="$current_path/tool/git/git.sh pull"
