ea377951ff7d15a970ee7778928253ff837036c9
hua_zhenguo <hua_zhenguo@163.com>
2020-06-23 09:20:06 +0800

    docker

--- soft/install/jad.sh
+++ soft/install/jad.sh
@@ -6,7 +6,7 @@ if [ ! -d $current_path/jad ]; then
     cd $current_path && wget https://varaneckas.com/jad/jad158g.mac.intel.zip
     unzip -n jad158g.mac.intel.zip -d jad/ 
     rm -rf $current_path/jad158g.mac.intel.zip
-    echo '####  jad环境变量' >> $current_path/tool/base.sh
+    echo '####  jad环境变量' >> $current_path/../tool/base.sh
     echo "JAD_HOME=$current_path/jad" >> $current_path/tool/base.sh
     echo 'export PATH=$JAD_HOME:$PATH'>> $current_path/tool/base.sh
     echo '############## end #############' >> $current_path/tool/base.sh
