d3468a3e2c25bc8d7645ad35d3e40685c300da8f
huazhenguo <hua_zhenguo@163.com>
2020-08-27 21:44:02 +0800

    begin

---
+++ single/elasticsearch/begin.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+mkdir -pv ../data/elasticsearch/conf/
+
+cd ../data/elasticsearch/conf/
+
+echo 'cluster.name: elasticsearch' > elasticsearch.yml
+echo 'network.host: 0.0.0.0' > elasticsearch.yml
