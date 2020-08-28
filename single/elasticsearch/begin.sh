fdbd965a010e58b2b085db02a17a4df66def4ed0
guozhenhua <guozhenhua7@jd.com>
2020-08-28 09:08:34 +0800

    修改

--- single/elasticsearch/begin.sh
+++ single/elasticsearch/begin.sh
@@ -6,3 +6,12 @@ cd ../data/elasticsearch/conf/
 
 echo 'cluster.name: elasticsearch' >> elasticsearch.yml
 echo 'network.host: 0.0.0.0' >> elasticsearch.yml
+
+
+cd ../data/kibana/conf/
+
+echo 'elasticsearch.hosts: "http://es:9200"' >> kibana.yml
+echo 'server.name: kibana' >> kibana.yml
+echo 'server.host: "0"' >> kibana.yml
+
+
