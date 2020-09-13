b82672f66ccab5a1146a4cc9d4906771742c32cc
huazhenguo <hua_zhenguo@163.com>
2020-09-13 16:35:57 +0800

    修改

--- tool/base.sh
+++ tool/base.sh
@@ -7,9 +7,6 @@ then
 elif [ "$(expr substr $(uname -s) 1 5)"=="Linux" ]
 then
     env_path="`pwd`"/"`dirname $0`"
-elif [ "$(expr substr $(uname -s) 1 10)"=="MINGW32_NT" ]
-then
-
 fi
 
 ####  migrate环境变量
