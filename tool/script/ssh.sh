649fbbed4b39957b065ebf568119bc6f308ae0cc
huazhenguo <hua_zhenguo@163.com>
2020-10-25 21:58:06 +0800

    ssh

--- tool/script/ssh.sh
+++ tool/script/ssh.sh
@@ -8,5 +8,8 @@ alias sshali="ssh root@39.98.143.124"
 alias sshtx="ssh root@193.112.56.221"
 
 alias ssh6="ssh root@192.168.18.236"
+alias ssh6h= "ssh -oPort=6600 root@39.98.143.124"
+
 alias ssh9="ssh root@192.168.18.183"
+alias ssh9h="ssh -oPort=9900 root@39.98.143.124"
 
