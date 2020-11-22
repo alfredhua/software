9a55b60beb6d46f09178e2807a3755aa58577e91
huazhenguo <hua_zhenguo@163.com>
2020-11-22 20:52:09 +0800

    修改

--- tool/project/manage.sh
+++ tool/project/manage.sh
@@ -4,7 +4,7 @@ PROJECT_HOME="/Users/hua/workspace/self"
 MANAGE_HOME=$PROJECT_HOME"/manage"
 
 alias mpro="cd $PROJECT_HOME"
-alias madmin="cd $MANAGE_HOME"
+alias mad="cd $MANAGE_HOME"
 alias mact="cd $MANAGE_HOME/server && ./gradlew :web:web-actuator:bootRun "
 alias mservice="cd $MANAGE_HOME/server && ./gradlew :web:web-service:bootRun "
 alias msite="cd $MANAGE_HOME/server && ./gradlew :web:web-site:bootRun "
