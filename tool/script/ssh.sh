cddadb158dba7be6f2335e7980ea8b432c607de5
huazhenguo <hua_zhenguo@163.com>
2020-11-25 22:59:42 +0800

    修改脚本

--- tool/script/ssh.sh
+++ tool/script/ssh.sh
@@ -8,8 +8,8 @@ alias sshali="ssh root@39.98.143.124"
 alias sshtx="ssh root@193.112.56.221"
 
 alias ssh6="ssh root@192.168.18.236"
-alias ssh6h= "ssh -oPort=6600 root@39.98.143.124"
-
 alias ssh9="ssh root@192.168.18.183"
-alias ssh9h="ssh -oPort=9900 root@39.98.143.124"
+
+alias sshpai="ssh root@192.168.18.149"
+alias sshparh="ssh -oPort=6000 root@39.98.143.124"
 
