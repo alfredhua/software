487f4fd0044426e21642752e19e78f03ecb97f83
huazhenguo <hua_zhenguo@163.com>
2020-12-26 22:41:41 +0800

    base

--- tool/base.sh
+++ tool/base.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-ALL_PATH="/Users/hua/workspace/software/soft"
+ALL_PATH="/Users/hua/software/soft"
 
 source $ALL_PATH/path.sh
 
 ## 快捷脚本
 
-env_path="/Users/hua/workspace/software/tool"
+env_path="/Users/hua/software/tool"
 
 ##docker 相关
 source $env_path/script/docker.sh
