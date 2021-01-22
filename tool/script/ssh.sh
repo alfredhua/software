80e9ddcdd6cb458cbe4d2c9563bb057f1c9e0833
huazhenguo <hua_zhenguo@163.com>
2021-01-22 14:39:06 +0800

    修改wifi

--- tool/script/ssh.sh
+++ tool/script/ssh.sh
@@ -7,8 +7,8 @@ alias sshali="ssh root@39.98.143.124"
 # 腾讯服务器
 alias sshtx="ssh root@193.112.56.221"
 
-alias ssh6="ssh root@192.168.18.236"
-alias ssh9="ssh root@192.168.18.183"
+alias ssh6="ssh root@192.168.0.106"
+alias ssh9="ssh root@192.168.0.109"
 
 alias sshpai="ssh root@192.168.18.149"
 alias sshparh="ssh -oPort=6000 root@39.98.143.124"
