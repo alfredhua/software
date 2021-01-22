9bf6a49ef95f2ceb9896ac9ffd719c7301087bd1
huazhenguo <hua_zhenguo@163.com>
2021-01-22 14:40:29 +0800

    修改

--- tool/script/ssh.sh
+++ tool/script/ssh.sh
@@ -7,7 +7,7 @@ alias sshali="ssh root@39.98.143.124"
 # 腾讯服务器
 alias sshtx="ssh root@193.112.56.221"
 
-alias ssh6="ssh root@192.168.0.106"
+alias ssh6="ssh root@192.168.0.105"
 alias ssh9="ssh root@192.168.0.109"
 
 alias sshpai="ssh root@192.168.18.149"
