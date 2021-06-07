007dcda7f745e8144ea3d3edef9fa243e64d9891
guozhenhua7 <guozhenhua7@jd.com>
2021-06-07 13:33:33 +0800

    pagth

--- soft/path.sh
+++ soft/path.sh
@@ -36,7 +36,14 @@ export PATH=$HIVE_HOME/bin:$PATH
 
 ## hadoop
 export HADOOP_HOME=$soft_path/hadoop
-export PATH=$HADOOP_HOME/bin:$PATH
+export HADOOP_MAPRED_HOME=$HADOOP_HOME 
+export HADOOP_COMMON_HOME=$HADOOP_HOME 
+export HADOOP_HDFS_HOME=$HADOOP_HOME 
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native 
+export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin/
+
+
 
 #### node 
 export NVM_DIR="$HOME/.nvm"
