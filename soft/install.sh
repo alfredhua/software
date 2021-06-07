86e7193817b2e3d9ea99cb848c4434aea965c364
guozhenhua7 <guozhenhua7@jd.com>
2021-06-07 10:44:36 +0800

    install

--- soft/install.sh
+++ soft/install.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-## mac环境下：执行./tool/install.sh即可
-
-tool_dir=`pwd`;
-
 # gradle 安装
-$tool_dir/install/gradle.sh
+./install/gradle.sh
 
 # jad 安装
-$tool_dir/install/jad.sh
+./install/jad.sh
 
 # maven 安装
-$tool_dir/install/maven.sh
+./install/maven.sh
 
 # mybatis_migration
-$tool_dir/install/migrateions.sh
+./install/migrateions.sh
 
 ## node 按照
-$tool_dir/install/node.sh
+./install/node.sh
 
 # jdk8安装
-$tool_dir/install/openjdk8.sh
+./install/openjdk8.sh
 
 # 钉钉内网穿透工具
-$tool_dir/install/pierced.sh
+./install/pierced.sh
 
