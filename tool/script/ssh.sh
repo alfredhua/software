611f309e247deb6b4adbd3c6e30ee5f12233a253
huazhenguo <hua_zhenguo@163.com>
2021-06-13 20:21:57 +0800

    修改ssh

--- tool/script/ssh.sh
+++ tool/script/ssh.sh
@@ -4,9 +4,9 @@
 # 阿里服务器
 alias sshali="ssh root@39.98.143.124"
 
-# 腾讯服务器
-alias sshtx="ssh root@193.112.56.221"
+# 家里服务器
+alias ssh105="ssh root@192.168.0.105"
+alias ssh105y="ssh -oPort=6000 root@39.98.143.124"
 
-alias ssh6="ssh root@192.168.0.105"
-alias ssh9="ssh root@192.168.0.107"
+alias ssh107="ssh root@192.168.0.107"
 
