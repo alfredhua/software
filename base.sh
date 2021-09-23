9bbe12bb930810e69238b5815e39a2d8dae5203b
huazhenguo <hua_zhenguo@163.com>
2021-09-23 22:24:46 +0800

    base

--- base.sh
+++ base.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source $SOFTWARE_PATH/soft/path.sh
+source $HOME/software/soft/path.sh
 
 ## MAVEN
 alias mc="mvn clean "
@@ -12,7 +12,7 @@ alias md="mvn dependency:tree"
 ## GIT
 alias gstatus="git status"
 alias gpull="git pull"
-alias gpush="push $1"
+alias gpush="git_push" 
 alias gdiff="git diff"
 alias gcheckout="git checkout"
 alias gmerge="git merge"
@@ -25,16 +25,10 @@ alias glog="git log"
 alias gadd="git add"
 alias gclone="git clone"
 alias gshow="git show"
-alias gporigin="push_origin $1"
+alias gporigin="git push --set-upstream origin $1"
 
-function push_origin(){
-   git push --set-upstream origin $1    
-}
-
-function push(){
-    git add .
-    git commit -m $1
-    git push
+function git_push(){
+    git add . && git commit -m $1 && git push
 }
 
 ## CENTOS
@@ -44,32 +38,18 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
+
 alias sshali="ssh root@39.98.143.124"
 alias ssh105="ssh root@192.168.0.105"
 alias ssh105y="ssh -oPort=6000 root@39.98.143.124"
 alias ssh107="ssh root@192.168.0.107"
 
 ## DOCKER
-alias denter="enter"
-alias compstart="compose_start"
-alias comstop="compose_stop"
-
-function enter(){
-    docker exec -it $1 bash
-}
-
-function compose_start(){
-   docker-compose -f $1 up -d
-}
-
-function compose_stop(){
-   docker-compose -f $1 down
-}
+alias denter="docker exec -it $1 bash"
+alias dstart="docker-compose -f $1 up -d"
+alias dstop="docker-compose -f $1 down"
 
 ## NODE 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 alias nrs="npm run start"
 alias nrdev="npm run dev"
@@ -92,7 +72,8 @@ alias py="python3"
 alias pip="pip3"
 
 ## 项目管理相关
-MANAGE_HOME="~/workspace/self/project"
+
+MANAGE_HOME="$HOME/workspace/self/project"
 alias jm="cd $MANAGE_HOME"
 alias msiterun="cd $MANAGE_HOME/server && ./gradlew :web:web-site:bootRun "
 alias madminrun="cd $MANAGE_HOME/server && ./gradlew :web:web-admin:bootRun "
--- soft/path.sh
+++ soft/path.sh
@@ -1,37 +1,48 @@
 #!/bin/sh
 
-####  gradle环境变量
+SOFT_WARE_PATH=$HOME/software/soft
 
-soft_path=$SOFTWARE_PATH/soft
-
-GRADLE_HOME=$soft_path/gradle/bin
-export PATH=$GRADLE_HOME:$PATH
-
-####  jad环境变量
-JAD_HOME=$soft_path/jad
-export PATH=$JAD_HOME:$PATH
+source $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 
-####  migrate环境变量
-MIGRATIONS_HOME=$soft_path/mybatis-migrations
-export MIGRATIONS=$MIGRATIONS_HOME/bin
-export PATH=$MIGRATIONS:$PATH
+## vim
+export VIM=$SOFT_WARE_PATH/vim/nvim/share/nvim
+export PATH=$PATH:$VIM:$SOFT_WARE_PATH/vim/nvim/bin
+
+#### node 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 ####  maven环境变量
-MAVEN_HOME=$soft_path/maven
+MAVEN_HOME=$SOFT_WARE_PATH/maven
 export MAVEN=$MAVEN_HOME/bin
 export PATH=$MAVEN:$PATH
 
+####  gradle环境变量
+GRADLE_HOME=$SOFT_WARE_PATH/gradle/bin
+export PATH=$GRADLE_HOME:$PATH
+
+####  jad环境变量
+JAD_HOME=$SOFT_WARE_PATH/jad
+export PATH=$JAD_HOME:$PATH
+
+####  migrate环境变量
+MIGRATIONS_HOME=$SOFT_WARE_PATH/mybatis-migrations
+export MIGRATIONS=$MIGRATIONS_HOME/bin
+export PATH=$MIGRATIONS:$PATH
+
+
 ## SPARK
-export SPARK_HOME=$soft_path/spark
+export SPARK_HOME=$SOFT_WARE_PATH/spark
 export PATH=$SPARK_HOME/bin:$PATH
 
 ## hive
-export HIVE_HOME=$soft_path/hive
+export HIVE_HOME=$SOFT_WARE_PATH/hive
 export PATH=$HIVE_HOME/bin:$PATH
 
 ## hadoop
-export HADOOP_HOME=$soft_path/hadoop
+export HADOOP_HOME=$SOFT_WARE_PATH/hadoop
 export HADOOP_MAPRED_HOME=$HADOOP_HOME 
 export HADOOP_COMMON_HOME=$HADOOP_HOME 
 export HADOOP_HDFS_HOME=$HADOOP_HOME 
@@ -40,32 +51,23 @@ export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
 export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin/
 
 ## hbase
-export HBASE_HOME=$soft_path/hbase
+export HBASE_HOME=$SOFT_WARE_PATH/hbase
 export PATH=$PATH:$HBASE_HOME/bin/
 
 ## JAVA
-
 name=`uname`
 if [ $name != 'Darwin' ]
 then
-    export JAVA_HOME=$soft_path/java
+    export JAVA_HOME=$SOFT_WARE_PATH/java
     export PATH=$PATH:$JAVA_HOME/bin/
 else
 fi
 
-## vim
-export VIM=$soft_path/vim/nvim/share/nvim
-export PATH=$PATH:$VIM:$soft_path/vim/nvim/bin
-
 ## scala
-export SCALA_HOME=$soft_path/scala
+export SCALA_HOME=$SOFT_WARE_PATH/scala
 export PATH=$PATH:$SCALA_HOME/bin/
 
 
 ## flink
-export FLINK_HOME=$soft_path/flink
+export FLINK_HOME=$SOFT_WARE_PATH/flink
 export PATH=$PATH:$FLINK_HOME/bin/
-
-#### node 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
