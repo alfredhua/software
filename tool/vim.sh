97446b186407e8a0299b6ee8d0c5b42bd464472e
guozhenhua7 <guozhenhua7@jd.com>
2021-11-09 10:28:15 +0800

    vim

--- tool/vim.sh
+++ tool/vim.sh
@@ -4,5 +4,5 @@ alias vi="nvim"
 alias vim="nvim"
 alias vihost="sudo vim /etc/hosts"
 alias viInit="vim $HOME/software/soft/vim/init.vim"
-alias viIdea="vim $HOME/software/soft/vim/ideavimrc.vim"
+alias viIdea="vim $HOME/software/soft/vim/ideavimrc"
 
