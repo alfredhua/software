2b06d4c1382a8c527a05599f6a2d98b626e9023c
huazhenguo <hua_zhenguo@163.com>
2021-11-27 18:33:09 +0800

    readme

--- README.md
+++ README.md
@@ -1,29 +1,3 @@
 # 配置
 
-## 环境变量
-
-> source $HOME/software/base.sh
-
-## 软件安装
-
-- brew install neovim
-- brew install zsh-syntax-highlighting
-- brew install fzf
-- brew install ack
-- pip3 install ranger-fm
-- pip3 install pynvim
-
-## VIM 相关
-
-- ln -s ~/software/soft/nvim/init.vim   ~/.config/nvim/init.vim
-- ln -s ~/software/soft/nvim/init.vim  ~/.ideavimrc
-- vim 下安装所有插件： PlugInstall
-
-## brew
-
-- /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-## ZSH
-
-- sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-- cd $HOME/.oh-my-zsh/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+执行 tool/init.sh	
--- tool/init.sh
+++ tool/init.sh
@@ -5,20 +5,16 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 cd $HOME/.oh-my-zsh/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
-echo "source $HOME/software/base.sh " >> $HOME/.zshr 
+echo "source $HOME/software/base.sh " >> $HOME/.zshrc
 
 ## brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install neovim
-brew install zsh-syntax-highlighting
-brew install fzf
-brew install ack
-## 函数跳转
-brew install ctags-exuberant
-# 文件对比
-brew install diff-so-fancy
+brew install neovim zsh-syntax-highlighting fzf ack ctags-exuberant diff-so-fancy
+
+pip3 install ranger-fm pynvim
+
 ## vim
-ln -s ~/software/soft/vim/init.vim   ~/.config/nvim/init.vim
-ln -s ~/software/soft/vim/ideavimrc  ~/.ideavimr
+ln -s ~/software/soft/nvim/init.vim   ~/.config/nvim/init.vim
+ln -s ~/software/soft/nvim/init.vim  ~/.ideavimrc
 
