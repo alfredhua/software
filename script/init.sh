af677e52bb83ae50253897cd449b8899d1b5e851
huazhenguo <hua_zhenguo@163.com>
2021-12-05 12:49:14 +0800

    init

--- script/init.sh
+++ script/init.sh
@@ -7,14 +7,17 @@ cd $HOME/.oh-my-zsh/plugins && git clone https://github.com/zsh-users/zsh-syntax
 
 echo "source $HOME/software/base.sh " >> $HOME/.zshrc
 
+# change zsh theme ZSH_THEME="awesomepanda"
+
 ## brew
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install neovim zsh-syntax-highlighting fzf ack ctags-exuberant diff-so-fancy
+brew install zsh-syntax-highlighting fzf ack ctags-exuberant diff-so-fancy
 
 pip3 install ranger-fm pynvim
 
 ## vim
-ln -s ~/software/soft/nvim/init.vim   ~/.config/nvim/init.vim
-ln -s ~/software/soft/nvim/init.vim  ~/.ideavimrc
+ln -s ~/software/config/nvim/init.vim   ~/.config/nvim/init.vim
+ln -s ~/software/config/nvim/init.vim  ~/.ideavimrc
 
