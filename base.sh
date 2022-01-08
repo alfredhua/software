9cc053bef587588608b8501bebc2efd9c428dff6
huazhenguo <hua_zhenguo@163.com>
2022-01-08 23:25:48 +0800

    a

--- base.sh
+++ base.sh
@@ -2,7 +2,7 @@
 source $HOME/software/soft/path.sh
 source $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-source $HOME/software/script/alias.sh
+#source $HOME/software/script/alias.sh
 source $HOME/software/script/deploy.sh
 
 # Shell
