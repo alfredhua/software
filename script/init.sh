b361ef66e4bf49c5c9cce891c01e5c5deb11cdf9
guozhenhua7 <guozhenhua7@jd.com>
2022-01-27 14:49:44 +0800

    init

--- script/init.sh
+++ script/init.sh
@@ -10,13 +10,13 @@ echo "source $HOME/software/base.sh " >> $HOME/.zshrc
 # change zsh theme ZSH_THEME="awesomepanda"
 
 ## brew
-
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew install zsh-syntax-highlighting fzf ack ctags-exuberant diff-so-fancy ripgrep
 
 pip3 install ranger-fm pynvim
 
+curl -Ls https://raw.githubusercontent.com/daipeihust/im-select/master/install_mac.sh | sh
 ## vim
 ln -s ~/software/config/nvim/init.vim   ~/.config/nvim/init.vim
 ln -s ~/software/config/nvim/init.vim  ~/.ideavimrc
