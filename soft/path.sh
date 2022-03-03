b63d66d83ee1dc01ccd82751b229c3ed8cedac96
guozhenhua7 <guozhenhua7@jd.com>
2022-03-03 09:33:58 +0800

    path

--- soft/path.sh
+++ soft/path.sh
@@ -23,45 +23,45 @@ GRADLE_HOME=$SOFT_WARE_PATH/gradle/bin
 export PATH=$GRADLE_HOME:$PATH
 
 ####  jad环境变量
-JAD_HOME=$SOFT_WARE_PATH/jad
-export PATH=$JAD_HOME:$PATH
+# JAD_HOME=$SOFT_WARE_PATH/jad
+# export PATH=$JAD_HOME:$PATH
 
 ####  migrate环境变量
-MIGRATIONS_HOME=$SOFT_WARE_PATH/mybatis-migrations
-export MIGRATIONS=$MIGRATIONS_HOME/bin
-export PATH=$MIGRATIONS:$PATH
+# MIGRATIONS_HOME=$SOFT_WARE_PATH/mybatis-migrations
+# export MIGRATIONS=$MIGRATIONS_HOME/bin
+# export PATH=$MIGRATIONS:$PATH
 
 
 ## SPARK
-export SPARK_HOME=$SOFT_WARE_PATH/spark
-export PATH=$SPARK_HOME/bin:$PATH
+# export SPARK_HOME=$SOFT_WARE_PATH/spark
+# export PATH=$SPARK_HOME/bin:$PATH
 
 ## hive
-export HIVE_HOME=$SOFT_WARE_PATH/hive
-export PATH=$HIVE_HOME/bin:$PATH
+# export HIVE_HOME=$SOFT_WARE_PATH/hive
+# export PATH=$HIVE_HOME/bin:$PATH
 
 ## hadoop
-export HADOOP_HOME=$SOFT_WARE_PATH/hadoop
-export HADOOP_MAPRED_HOME=$HADOOP_HOME 
-export HADOOP_COMMON_HOME=$HADOOP_HOME 
-export HADOOP_HDFS_HOME=$HADOOP_HOME 
-export YARN_HOME=$HADOOP_HOME
-export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native 
-export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin/
+# export HADOOP_HOME=$SOFT_WARE_PATH/hadoop
+# export HADOOP_MAPRED_HOME=$HADOOP_HOME 
+# export HADOOP_COMMON_HOME=$HADOOP_HOME 
+# export HADOOP_HDFS_HOME=$HADOOP_HOME 
+# export YARN_HOME=$HADOOP_HOME
+# export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native 
+# export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin/
 
 ## hbase
-export HBASE_HOME=$SOFT_WARE_PATH/hbase
-export PATH=$PATH:$HBASE_HOME/bin/
+# export HBASE_HOME=$SOFT_WARE_PATH/hbase
+# export PATH=$PATH:$HBASE_HOME/bin/
 
 
 ## scala
-export SCALA_HOME=$SOFT_WARE_PATH/scala
-export PATH=$PATH:$SCALA_HOME/bin/
+# export SCALA_HOME=$SOFT_WARE_PATH/scala
+# export PATH=$PATH:$SCALA_HOME/bin/
 
 
 ## flink
-export FLINK_HOME=$SOFT_WARE_PATH/flink
-export PATH=$PATH:$FLINK_HOME/bin/
+# export FLINK_HOME=$SOFT_WARE_PATH/flink
+# export PATH=$PATH:$FLINK_HOME/bin/
 
 ## jdk8
 export JAVA_8_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_311.jdk/Contents/Home
