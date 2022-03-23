2f8dfcb043dd134a32eb732c115fff1a30ccc46b
guozhenhua7 <guozhenhua7@jd.com>
2022-03-23 19:46:00 +0800

    script

--- script/init.sh
+++ script/init.sh
@@ -1,42 +1,15 @@
 #!/bin/sh
-
+source $HOME/software/script/install.sh
 #===========  修改程序图标排列个数 =======================
 function change_dock(){
 	defaults write com.apple.dock springboard-columns -int 11 && defaults write com.apple.dock ResetLaunchPad -bool true && killall Dock
 }
 
-#===========  brew =======================
-function install_brew(){
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-	brew install neovim zsh-syntax-highlighting fzf ack ctags-exuberant diff-so-fancy ripgrep n
-}
+install_pierced
 
-#===========  zsh =======================
-#change zsh theme ZSH_THEME="awesomepanda"
-function install_zsh(){
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	cd $HOME/.oh-my-zsh/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-	echo "source $HOME/software/base.sh " >> $HOME/.zshrc
-}
 
-#===========  vim =======================
-function install_neovim(){
-	curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	curl -Ls https://raw.githubusercontent.com/daipeihust/im-select/master/install_mac.sh | sh
-	ln -s ~/software/config/nvim/init.vim   ~/.config/nvim/init.vim
-	ln -s ~/software/config/nvim/init.vim  ~/.ideavimrc
-	pip3 install ranger-fm pynvim
-}
-
-#===========  nvm =======================
-function install_nvm(){
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-	source $HOME/.zshrc
-	nvm install v12.22.10
-}
-
-change_dock
-install_brew
-install_zsh
-install_neovim
-install_nvm
+# change_dock
+# install_brew
+# install_zsh
+# install_neovim
+# install_nvm
--- script/install.sh
+++ script/install.sh
@@ -1,55 +1,14 @@
 #!/bin/sh
 
-down_load_path=$HOME/software/soft
-soft_list=(
-"maven,zip,apache-maven-3.8.3-bin.zip,apache-maven-3.8.3,https://dlcdn.apache.org/maven/maven-3/3.8.3/binaries/apache-maven-3.8.3-bin.zip"
-"gradle,zip,gradle-7.4-bin.zip,gradle-7.4,https://downloads.gradle-dn.com/distributions/gradle-7.4-bin.zip"
-"flink,tgz,flink-1.14.0-bin-scala_2.11.tgz,flink-1.14.0,https://dlcdn.apache.org/flink/flink-1.14.0/flink-1.14.0-bin-scala_2.11.tgz"
-"hadoop,gz,hadoop-3.2.2.tar.gz,hadoop-3.2.2,https://ftp.jaist.ac.jp/pub/apache/hadoop/common/hadoop-3.2.2/hadoop-3.2.2.tar.gz"
-"jad,zip,jad158g.mac.intel.zip,,https://varaneckas.com/jad/jad158g.mac.intel.zip"
-"scala,tgz,scala-2.11.1.tgz,scala-2.11.1,https://downloads.lightbend.com/scala/2.11.1/scala-2.11.1.tgz"
-)
+SOFT=$HOME/software/soft
 
-function install(){
-	echo ' begin install ' $1 
-	for soft in ${soft_list[@]};
-	do
-		soft_name=`echo ${soft}|awk -F[,] '{print $1}'`
-		if [ $soft_name == $1 ]
-		then
-			file_type=`echo ${soft}|awk -F[,] '{print $2}'`
-			file_name=`echo ${soft}|awk -F[,] '{print $3}'`
-			dic_name=`echo ${soft}|awk -F[,] '{print $4}'`
-			url=`echo ${soft}|awk -F[,] '{print $5}'`
-			echo $file_type $file_name $dic_name $url
-			if [ ! -d $down_load_path/$1 ]; then
-				echo "$1 download...."
-				cd $down_load_path && wget $url 
-				if [ "zip" == $file_type  ]; then
-					unzip -n $file_name -d $1
-				else
-					tar -xzvf $file_name  && mv $dic_name $1
-				fi
-				rm -rf $file_name 
-			fi
-		fi
-	done;
-	echo ' end ' $1
-}
-
-function node_install(){
-	brew install yarn
-	curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
-	nvm install v12.8.0
-	nvm use v12.8.0
-	nvm alias default v12.8.0
-}
-
-function pierced(){
-	current_path=$down_load_path
+#===========  piereced =======================
+function install_pierced(){
+	current_path=$SOFT
 	if [ ! -d $current_path/pierced ]; then
 	  cd $current_path
 	  git clone https://github.com/open-dingtalk/pierced.git
+	  touch $current_path/pierced/start.sh
 	  echo '#!/bin/sh' >> $current_path/pierced/start.sh
 	  echo '#####钉钉内网穿透启动脚本' >> $current_path/pierced/start.sh
 	  echo "./mac_64/ding -config=./mac_64/ding.cfg -subdomain=\$1 \$2">> $current_path/pierced/start.sh
@@ -57,13 +16,33 @@ function pierced(){
 	fi
 }
 
-for soft in $1
-do
-	if [ $soft == 'node' ];then
-		node_install
-	elif [ $soft == 'pierced' ];then
-		pierced
-	else
-		install $soft
-	fi
-done
+#===========  brew =======================
+function install_brew(){
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	brew install neovim zsh-syntax-highlighting fzf ack ctags-exuberant diff-so-fancy ripgrep n
+}
+
+#===========  zsh =======================
+#change zsh theme ZSH_THEME="awesomepanda"
+function install_zsh(){
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	cd $HOME/.oh-my-zsh/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+	echo "source $HOME/software/base.sh " >> $HOME/.zshrc
+}
+
+#===========  vim =======================
+function install_neovim(){
+	curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	curl -Ls https://raw.githubusercontent.com/daipeihust/im-select/master/install_mac.sh | sh
+	ln -s ~/software/config/nvim/init.vim   ~/.config/nvim/init.vim
+	ln -s ~/software/config/nvim/init.vim  ~/.ideavimrc
+	pip3 install ranger-fm pynvim
+}
+
+#===========  nvm =======================
+function install_nvm(){
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+	source $HOME/.zshrc
+	nvm install v12.22.10
+}
+
