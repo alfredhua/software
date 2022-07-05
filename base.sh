7b7267fc03253d99b2816797875fa5b8f17aa3f0
huazhenguo <hua_zhenguo@163.com>
2022-07-05 22:34:45 +0800

    base

--- base.sh
+++ base.sh
@@ -3,9 +3,5 @@ source $HOME/software/soft/path.sh
 source $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 source $HOME/software/script/alias.sh
-source $HOME/software/script/pro.sh
-source $HOME/software/script/deploy.sh
 
-# Shell
-alias sh_clear_sclipse='$HOME/software/script/clear_eclipse_temp_file.sh'
-
+export install=$HOME/software/script/install/
