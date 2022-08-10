8562486f67d5f6d7c29e7c79bd3bfd0b6fed87fd
guozhenhua7 <guozhenhua7@jd.com>
2022-08-10 15:04:16 +0800

    ranger

---
+++ script/install/ranger.sh
@@ -0,0 +1,10 @@
+function install_ranger(){
+    # brew install ranger
+    rm -rf ~/.config/ranger && mkdir ~/.config/ranger
+    echo 'mime ^text,  label editor = nvim -- "$@"' >> ~/.config/ranger/rifle.conf
+    echo 'mime ^text,  label pager  = "$PAGER" -- "$@"' >> ~/.config/ranger/rifle.conf
+    echo '!mime ^text, label editor, ext xml|json|csv|tex|py|pl|rb|js|sh|php = nvim -- "$@"' >> ~/.config/ranger/rifle.conf
+    echo '!mime ^text, label pager,  ext xml|json|csv|tex|py|pl|rb|js|sh|php = "$PAGER" -- "$@"' >> ~/.config/ranger/rifle.conf
+}
+
+install_ranger
