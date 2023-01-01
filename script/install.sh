fe34abdaa0840f8f8e3fd543590f25efd0dfaa12
hua_zhenguo <hua_zhenguo@163.com>
2023-01-01 21:10:22 +0800

    修改

--- script/install.sh
+++ script/install.sh
@@ -1,66 +1,56 @@
-#=========== brew =========
-alias i_brew="install_brew"
-function install_brew(){
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-}
+# /usr/bin
+current_path=$HOME/software/soft
+# homebrew
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
 
-#=========== arthas =========
-alias i_arthas="install_arthas"
-function install_arthas(){
-  curl -fLo $HOME/software/soft/arthas/arthas-boot.jar --create-dirs  https://arthas.aliyun.com/arthas-boot.jar
-}
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# /bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
 
-#=========== neovim =========
-alias i_neovim="install_neovim"
-function install_neovim(){
-  brew install neovim ctags
-  curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  ln -s ~/software/config/nvim/init.vim   ~/.config/nvim/init.vim
-  ln -s ~/software/config/nvim/init.vim  ~/.ideavimrc
-  curl -Ls https://raw.githubusercontent.com/daipeihust/im-select/master/install_mac.sh | sh
-}
+# jdk8
+brew install --cask homebrew/cask-versions/adoptopenjdk8
 
-#===========  nvm =======================
-alias i_nvm="install_nvm"
-function install_nvm(){
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-	source $HOME/.zshrc
-	nvm install v12.22.10
-}
+brew install git maven ctags neovim nvm python3
 
-#===========  piereced =======================
-alias i_pierced="install_pierced"
-function install_pierced(){
-	current_path=$HOME/software/soft
-	if [ ! -d $current_path/pierced ]; then
-	  cd $current_path
-	  git clone https://github.com/open-dingtalk/pierced.git
-	  touch $current_path/pierced/start.sh
-	  echo '#!/bin/sh' >> $current_path/pierced/start.sh
-	  echo '#####钉钉内网穿透启动脚本' >> $current_path/pierced/start.sh
-	  echo "./mac_64/ding -config=./mac_64/ding.cfg -subdomain=\$1 \$2">> $current_path/pierced/start.sh
-	  chmod -R 755 $current_path/pierced/start.sh
-	fi
-}
+# ohmyzsh
+chsh -s $(which zsh)
 
-#===========  i_ranger =======================
-alias i_ranger="install_ranger"
-function install_ranger(){
-    pip3 install ranger-fm pynvim
-    rm -rf ~/.config/ranger && mkdir ~/.config/ranger
-    echo 'mime ^text,  label editor = nvim -- "$@"' >> ~/.config/ranger/rifle.conf
-    echo 'mime ^text,  label pager  = "$PAGER" -- "$@"' >> ~/.config/ranger/rifle.conf
-    echo '!mime ^text, label editor, ext xml|json|csv|tex|py|pl|rb|js|sh|php = nvim -- "$@"' >> ~/.config/ranger/rifle.conf
-    echo '!mime ^text, label pager,  ext xml|json|csv|tex|py|pl|rb|js|sh|php = "$PAGER" -- "$@"' >> ~/.config/ranger/rifle.conf
-}
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-#===========  zsh =======================
-#change zsh theme ZSH_THEME="awesomepanda"
-alias i_zsh="install_zsh"
-function install_zsh(){
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	cd $HOME/.oh-my-zsh/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-	brew install zsh-syntax-highlighting
-	echo "source $HOME/software/base.sh " >> $HOME/.zshrc
-}
+cd $HOME/.oh-my-zsh/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+echo "source ~/software/base.sh " >> $HOME/.zshrc
+
+# neovim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -s ~/software/config/nvim/init.vim   ~/.config/nvim/init.vim
+
+ln -s ~/software/config/nvim/init.vim  ~/.ideavimrc
+
+curl -Ls https://raw.githubusercontent.com/daipeihust/im-select/master/install_mac.sh | sh
+
+#nvm 
+cd ~ && mkdir ~/.nvm
+echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.zshrc
+echo '    [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && \. "$(brew --prefix)/opt/nvm/nvm.sh" # This loads nvm ' >> $HOME/.zshrc
+echo '    [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm"' >> $HOME/.zshrc
+
+source ~/.zshrc
+
+# arthas
+curl -fLo $HOME/software/soft/arthas/arthas-boot.jar --create-dirs  https://arthas.aliyun.com/arthas-boot.jar
+
+#node 
+nvm install v12.22.12
+nvm install v14.14.0
+nvm alias default v14.14.0
+ 
+# nvim ranger vim中图片预览工具
+pip3 install ranger-fm pynvim
+
+rm -rf ~/.config/ranger && mkdir ~/.config/ranger
+
+echo 'mime ^text,  label editor = nvim -- "$@"' >> ~/.config/ranger/rifle.conf
+echo 'mime ^text,  label pager  = "$PAGER" -- "$@"' >> ~/.config/ranger/rifle.conf
+echo '!mime ^text, label editor, ext xml|json|csv|tex|py|pl|rb|js|sh|php = nvim -- "$@"' >> ~/.config/ranger/rifle.conf
 
