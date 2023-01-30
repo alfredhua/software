318226771c9dc155a349d87d408f03e9ac9eef9b
guozhenhua7 <guozhenhua7@jd.com>
2023-01-30 10:17:16 +0800

    修改install

--- script/install.sh
+++ script/install.sh
@@ -1,28 +1,30 @@
 # /usr/bin
 
-# homebrew
+# 设置代理
 # export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
 
+# 安装homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # /bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
 
-# jdk8
-brew install --cask homebrew/cask-versions/adoptopenjdk8
+# 安装软件
+brew install git maven ctags neovim nvm python3 rg wget autojump
 
-brew install git maven ctags neovim nvm python3 rg wget
+# 安装jdk8
+brew install --cask homebrew/cask-versions/adoptopenjdk8
 
 echo "export JAVA_HOME=\$(/usr/libexec/java_home)" >>$HOME/.zshrc
 
-# ohmyzsh
+# 安装ohmyzsh
+
 chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 cd $HOME/.oh-my-zsh/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
-# 下载插件
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-## 主题
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 rm $HOME/.zshrc && ln -s ~/software/config/zsh/zshrc $HOME/.zshrc
@@ -31,18 +33,17 @@ rm $HOME/.zshrc && ln -s ~/software/config/zsh/zshrc $HOME/.zshrc
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s ~/software/config/nvim/neovim/init.vim  ~/.config/nvim/init.vim
-##idea
+
+# idea 快捷键设置
 ln -s ~/software/config/nvim/idea/ideavimrc ~/.ideavimrc
 
+# 输入法自动切换
 curl -Ls https://raw.githubusercontent.com/daipeihust/im-select/master/install_mac.sh | sh
 
-#nvm 
+# nvm 
 cd ~ && mkdir ~/.nvm
     
 source ~/.zshrc
-
-# arthas
-curl -fLo $HOME/software/soft/arthas/arthas-boot.jar --create-dirs  https://arthas.aliyun.com/arthas-boot.jar
 
 #node 
 nvm install v12.22.12
@@ -58,5 +59,5 @@ echo 'mime ^text,  label editor = nvim -- "$@"' >> ~/.config/ranger/rifle.conf
 echo 'mime ^text,  label pager  = "$PAGER" -- "$@"' >> ~/.config/ranger/rifle.conf
 echo '!mime ^text, label editor, ext xml|json|csv|tex|py|pl|rb|js|sh|php = nvim -- "$@"' >> ~/.config/ranger/rifle.conf
 
-
-
+# arthas
+curl -fLo $HOME/software/soft/arthas/arthas-boot.jar --create-dirs  https://arthas.aliyun.com/arthas-boot.jar
